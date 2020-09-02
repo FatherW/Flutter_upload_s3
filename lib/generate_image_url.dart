fe1d5bc02b6b320613c0a5827956691e15f20f42
FatherW <673051389@qq.com>
2020-09-02 15:13:46 +0800

    Successfully connect to S3

--- lib/generate_image_url.dart
+++ lib/generate_image_url.dart
@@ -17,10 +17,12 @@ class GenerateImageUrl {
 
       var response = await http.post(
         //For IOS
-        'http://localhost:5000/generatePresignedUrl',
+        //  'http://localhost:5000/generatePresignedUrl',
         //For Android
         //    'http://10.0.2.2:5000/generatePresignedUrl',
         //'https://0mx3cqjlkh.execute-api.ap-northeast-1.amazonaws.com/default/getPresignedURL',
+        //  "https://guoquan-upload.s3.ap-northeast-1.amazonaws.com/abcde.jpg?Content-Type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI6FDT6Z3WRGPB7QQ%2F20200902%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20200902T060249Z&X-Amz-Expires=3600&X-Amz-Signature=2ee1a6e2cfa4ddc7e9fbc81b3c8bc4e18c7a5cd3afd0518281148048fa8fc43c&X-Amz-SignedHeaders=host%3Bx-amz-acl&x-amz-acl=public-read",
+        "https://0mx3cqjlkh.execute-api.ap-northeast-1.amazonaws.com/default/getPresignedURL",
         body: body,
       );
 
@@ -31,7 +33,9 @@ class GenerateImageUrl {
       if (result['success'] != null) {
         success = result['success'];
         message = result['message'];
-
+        print(response.statusCode);
+//response.statusCode LOCAL的值是201
+//response.statusCode LOCAL的值是200
         if (response.statusCode == 200) {
           isGenerated = true;
           uploadUrl = result["uploadUrl"];
--- lib/upload_file.dart
+++ lib/upload_file.dart
@@ -9,8 +9,12 @@ class UploadFile {
 
   Future<void> call(String url, File image) async {
     try {
-      var response = await http.put(url, body: image.readAsString());
-      print(image.readAsString());
+      // var url =
+      //     "https://guoquan-upload.s3.ap-northeast-1.amazonaws.com/abcde.jpg?Content-Type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI6FDT6Z3WRGPB7QQ%2F20200902%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20200902T060249Z&X-Amz-Expires=3600&X-Amz-Signature=2ee1a6e2cfa4ddc7e9fbc81b3c8bc4e18c7a5cd3afd0518281148048fa8fc43c&X-Amz-SignedHeaders=host%3Bx-amz-acl&x-amz-acl=public-read";
+      var response = await http.put(url, body: image.readAsBytesSync());
+      print(response.statusCode);
+//此处local的返回CODE是200
+//LAMBDA的返回值是403，原因未知
       if (response.statusCode == 200) {
         isUploaded = true;
       } else {
